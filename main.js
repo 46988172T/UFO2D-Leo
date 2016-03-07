bbbc8b45dd6268dd9ddc430ff383b9e032aa3508
Leosss <46988172t@iespoblenou.org>
2016-03-07 16:35:56 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,72 @@
+/// <reference path="phaser/phaser.d.ts"/>
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var mainState = (function (_super) {
+    __extends(mainState, _super);
+    function mainState() {
+        _super.apply(this, arguments);
+        this.UFO_SIZE = 75;
+        this.MAX_SPEED = 300; // pixels/second
+        this.ACCELERATION = 800; // pixels/second/second
+    }
+    mainState.prototype.preload = function () {
+        _super.prototype.preload.call(this);
+        this.load.image('ufo', 'assets/UFO.png');
+        this.load.image('pickup', 'assets/Pickup.png');
+        this.load.image('background', 'assets/Background.png');
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+    };
+    mainState.prototype.create = function () {
+        _super.prototype.create.call(this);
+        var background;
+        background = this.add.sprite(0, 0, 'background');
+        var scale = this.world.height / background.height;
+        background.scale.setTo(scale, scale);
+        this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
+        this.ufo.width = this.ufo.height = this.UFO_SIZE;
+        this.ufo.anchor.setTo(0.5, 0.5);
+        this.physics.enable(this.ufo, Phaser.Physics.ARCADE);
+        this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
+        this.ufo.body.collideWorldBounds = true;
+        this.ufo.body.bounce.set(0.3);
+        this.cursor = this.input.keyboard.createCursorKeys();
+    };
+    mainState.prototype.update = function () {
+        _super.prototype.update.call(this);
+        this.game.debug.bodyInfo(this.ufo, 0, 0);
+        if (this.cursor.left.isDown) {
+            this.ufo.body.acceleration.x = -this.ACCELERATION;
+        }
+        else if (this.cursor.right.isDown) {
+            this.ufo.body.acceleration.x = this.ACCELERATION;
+        }
+        else if (this.cursor.up.isDown) {
+            this.ufo.body.acceleration.y = -this.ACCELERATION;
+        }
+        else if (this.cursor.down.isDown) {
+            this.ufo.body.acceleration.y = this.ACCELERATION;
+        }
+        else {
+            this.ufo.body.acceleration.x = 0;
+            this.ufo.body.acceleration.y = 0;
+            this.ufo.body.velocity.x = 0;
+            this.ufo.body.velocity.y = 0;
+        }
+    };
+    return mainState;
+})(Phaser.State);
+var SimpleGame = (function () {
+    function SimpleGame() {
+        this.game = new Phaser.Game(600, 600, Phaser.AUTO, 'gameDiv');
+        this.game.state.add('main', mainState);
+        this.game.state.start('main');
+    }
+    return SimpleGame;
+})();
+window.onload = function () {
+    var game = new SimpleGame();
+};
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,75 @@
+/// <reference path="phaser/phaser.d.ts"/>
+
+class mainState extends Phaser.State {
+    private ufo:Phaser.Sprite;
+    private cursor:Phaser.CursorKeys;
+    private UFO_SIZE = 75;
+
+    private MAX_SPEED = 300; // pixels/second
+    private ACCELERATION = 800; // pixels/second/second
+
+    preload():void {
+        super.preload();
+
+        this.load.image('ufo', 'assets/UFO.png');
+        this.load.image('pickup', 'assets/Pickup.png');
+        this.load.image('background', 'assets/Background.png');
+
+        this.physics.startSystem(Phaser.Physics.ARCADE);
+    }
+
+    create():void {
+        super.create();
+        var background;
+
+        background = this.add.sprite(0, 0, 'background');
+        var scale = this.world.height / background.height;
+        background.scale.setTo(scale, scale);
+
+        this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
+        this.ufo.width = this.ufo.height = this.UFO_SIZE;
+        this.ufo.anchor.setTo(0.5, 0.5);
+
+        this.physics.enable(this.ufo, Phaser.Physics.ARCADE);
+        this.ufo.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED); // x, y
+        this.ufo.body.collideWorldBounds = true;
+        this.ufo.body.bounce.set(0.3);
+        this.cursor = this.input.keyboard.createCursorKeys();
+
+    }
+
+    update():void {
+        super.update();
+        this.game.debug.bodyInfo(this.ufo, 0, 0);
+
+        if (this.cursor.left.isDown) {
+            this.ufo.body.acceleration.x = -this.ACCELERATION;
+        } else if (this.cursor.right.isDown) {
+            this.ufo.body.acceleration.x = this.ACCELERATION;
+        } else if (this.cursor.up.isDown) {
+            this.ufo.body.acceleration.y = -this.ACCELERATION;
+        } else if (this.cursor.down.isDown) {
+            this.ufo.body.acceleration.y = this.ACCELERATION;
+        } else {
+            this.ufo.body.acceleration.x = 0;
+            this.ufo.body.acceleration.y = 0;
+            this.ufo.body.velocity.x = 0;
+            this.ufo.body.velocity.y = 0;
+        }
+    }
+}
+
+class SimpleGame {
+    game:Phaser.Game;
+
+    constructor() {
+        this.game = new Phaser.Game(600, 600, Phaser.AUTO, 'gameDiv');
+
+        this.game.state.add('main', mainState);
+        this.game.state.start('main');
+    }
+}
+
+window.onload = () => {
+    var game = new SimpleGame();
+};
